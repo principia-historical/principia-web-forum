caf533872d4760ea64a2b676519d71417441889d
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-05-23 21:46:03 +0200

    Rewrite index page to use Twig.

--- index.php
+++ index.php
@@ -23,8 +23,6 @@ if ($log && $action == 'markread') {
 	redirect('index.php');
 }
 
-pageheader(null,0);
-
 $categs = $sql->query("SELECT * FROM categories ORDER BY ord,id");
 while ($c = $categs->fetch()) {
 	$categ[$c['id']] = $c;
@@ -36,46 +34,9 @@ $forums = $sql->query("SELECT f.*, ".($log ? "r.time rtime, " : '').userfields('
 		. "LEFT JOIN categories c ON c.id=f.cat "
 		. ($log ? "LEFT JOIN forumsread r ON r.fid = f.id AND r.uid = ".$userdata['id'] : '')
 		. " ORDER BY c.ord,c.id,f.ord,f.id", []);
-$cat = -1;
 
-while ($forum = $forums->fetch()) {
-	if (!can_view_forum($forum)) continue;
-
-	if ($forum['cat'] != $cat) {
-		if ($cat != -1) echo '</table><br>';
-
-		$cat = $forum['cat'];
-		?>
-<table class="c1">
-	<tr class="h">
-		<td class="b h" width="17">&nbsp;</td>
-		<td class="b h"><?=$categ[$cat]['title'] ?></td>
-		<td class="b h" width="50">Threads</td>
-		<td class="b h" width="50">Posts</td>
-		<td class="b h" width="150">Last post</td>
-	</tr>
-<?php
-	}
-
-	if ($forum['posts'] > 0 && $forum['lastdate'] > 0)
-		$lastpost = sprintf(
-			'<nobr>%s</nobr><br><span class=sfont>by %s <a href="thread.php?pid=%s#%s">&raquo;</a></span>',
-		date($dateformat, $forum['lastdate']), userlink($forum, 'u'), $forum['lastid'], $forum['lastid']);
-	else
-		$lastpost = 'None';
-
-	$status = ($forum['lastdate'] > ($log ? $forum['rtime'] : time() - 3600) ? rendernewstatus("n") : '');
-
-	?><tr class="center">
-		<td class="b n1"><?=$status ?></td>
-		<td class="b n2 left">
-			<?=($forum['private'] ? '(' : '') ?><a href="forum.php?id=<?=$forum['id'] ?>"><?=$forum['title'] ?></a><?=($forum['private'] ? ')' : '') ?>
-			<br><span class="sfont"><?=str_replace("%%%RANDOM%%%", $randdesc[array_rand($randdesc)], $forum['descr']) ?></span>
-		</td>
-		<td class="b n1"><?=$forum['threads'] ?></td>
-		<td class="b n1"><?=$forum['posts'] ?></td>
-		<td class="b n2"><?=$lastpost ?></td>
-	</tr><?php
-}
-?></table><?php
-pagefooter();
+$twig = _twigloader();
+echo $twig->render('index.twig', [
+	'forums' => $forums,
+	'categories' => $categs
+]);
