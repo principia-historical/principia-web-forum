e808a394a0156f4b362da6db9c8558e2d98fdbd4
ROllerozxa <temporaryemail4meh+github@gmail.com>
2021-06-25 22:49:52 +0200

    Prevent creating a new thread with an empty message.

--- newthread.php
+++ newthread.php
@@ -15,9 +15,14 @@ if (!can_create_forum_thread($forum))
 
 $error = '';
 
+$title = isset($_POST['title']) ? $_POST['title'] : '';
+$message = isset($_POST['message']) ? $_POST['message'] : '';
+
 if ($action == 'Submit') {
-	if (strlen(trim($_POST['title'])) < 4)
+	if (strlen(trim($title)) < 0)
 		$error = "You need to enter a longer title.";
+	if (strlen(trim($message)) == 0)
+		$error = "You need to enter a message to your thread.";
 	if ($userdata['lastpost'] > time() - 30 && $action == 'Submit' && !has_perm('ignore-thread-time-limit'))
 		$error = "Don't post threads so fast, wait a little longer.";
 	if ($userdata['lastpost'] > time() - 2 && $action == 'Submit' && has_perm('ignore-thread-time-limit'))
@@ -28,14 +33,14 @@ if ($action == 'Submit') {
 			[time(), $userdata['id']]);
 
 		$sql->query("INSERT INTO threads (title, forum, user, lastdate, lastuser) VALUES (?,?,?,?,?)",
-			[$_POST['title'], $fid, $userdata['id'], time(), $userdata['id']]);
+			[$title, $fid, $userdata['id'], time(), $userdata['id']]);
 
 		$tid = $sql->insertid();
 		$sql->query("INSERT INTO posts (user, thread, date) VALUES (?,?,?)",
 			[$userdata['id'], $tid, time()]);
 
 		$pid = $sql->insertid();
-		$sql->query("INSERT INTO poststext (id, text) VALUES (?,?)", [$pid, $_POST['message']]);
+		$sql->query("INSERT INTO poststext (id, text) VALUES (?,?)", [$pid, $message]);
 
 		$sql->query("UPDATE forums SET threads = threads + 1, posts = posts + 1, lastdate = ?,lastuser = ?,lastid = ? WHERE id = ?",
 			[time(), $userdata['id'], $pid, $fid]);
@@ -50,9 +55,6 @@ $topbot = [
 	'breadcrumb' => [['href' => './', 'title' => 'Main'], ['href' => "forum.php?id=$fid", 'title' => $forum['title']]],
 	'title' => "New thread"
 ];
-
-$title = isset($_POST['title']) ? $_POST['title'] : '';
-$message = isset($_POST['message']) ? $_POST['message'] : '';
 
 if ($action == 'Preview') {
 	$post['date'] = time();
