6aa69e76b65d8f9814cfa8f567f1f8bedeed0c0e
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-02 19:31:35 +0200

    Move twig.php into layout.php

--- lib/layout.php
+++ lib/layout.php
@@ -113,3 +113,24 @@ function _twigloader($subfolder = '') {
 
 	return $twig;
 }
+
+class PrincipiaForumExtension extends \Twig\Extension\AbstractExtension {
+	public function getFunctions() {
+		return [
+			// datetime.php
+			new \Twig\TwigFunction('timeunits', 'timeunits', ['is_safe' => ['html']]),
+			new \Twig\TwigFunction('timelinks', 'timelinks', ['is_safe' => ['html']]),
+
+			// layout.php
+			new \Twig\TwigFunction('new_status', 'newStatus', ['is_safe' => ['html']]),
+			new \Twig\TwigFunction('render_page_bar', 'renderPageBar', ['is_safe' => ['html']]),
+			new \Twig\TwigFunction('if_empty_query', 'ifEmptyQuery', ['is_safe' => ['html']]),
+
+			// misc.php
+			new \Twig\TwigFunction('mlink', 'mlink', ['is_safe' => ['html']]),
+
+			// post.php
+			new \Twig\TwigFunction('threadpost', 'threadpost', ['is_safe' => ['html']]),
+		];
+	}
+}
